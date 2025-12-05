ce01705883ed861a624558766f911ac1da426ca3
drkdarkdrk <connorhampson4@gmail.com>
2025-12-05 13:11:27 +0000

    Add files via upload

---
+++ day4/day4-1.py
@@ -0,0 +1,48 @@
+with open("day4.txt","r") as day4_file:
+      data=[]
+      for line in day4_file:
+            item=[]
+            lineStrip=line.strip()
+            for entry in lineStrip:
+                  temp=[]
+                  temp.append(entry.strip())
+                  item.append(temp)
+            data.append(item)
+
+
+      for line in data:
+            line.insert(0,['.'])
+            line.append(['.'])
+
+      topBottomArray=[]
+      for i in range(len(data)+2):
+            topBottomArray.append(['.'])
+
+      data.insert(0,topBottomArray)
+      data.append(topBottomArray)
+
+      # for i in range(len(data)):
+      #       for j in range(len(data[i])):
+      #             print(data[i][j] ,end='')
+      #       print("")
+
+      total=0
+
+      for i in range(1,len(data)-1):
+            for j in range(1,len(data[i])-1):
+                  count=0
+                  if(data[i][j]==['@']):
+                        for p in range(-1,2):
+                              if(data[i+p][j-1]==["@"]):
+                                    count+=1
+                              if(data[i+p][j]==["@"]):
+                                    if(p!=0):
+                                          count+=1
+                              if(data[i+p][j+1]==["@"]):
+                                    count+=1
+                        if(count<4):
+                              total+=1
+      print(total)
+                  
+
+            
